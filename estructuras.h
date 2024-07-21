04a5a1d1fe7fb09df258d949ffd902f767d6837e
Guts <aamamanica@unjbg.edu.pe>
2024-07-21 18:02:53 -0500

    Se modifico el encabezado, pues salida biblioteca en vez de estructura

--- estructuras.h
+++ estructuras.h
@@ -1,7 +1,33 @@
-#ifndef BIBLIOTECA_H
-#define BIBLIOTECA_H
+#ifndef ESTRUCTURAS_H
+#define ESTRUCTURAS_H
 
+#include <string>
 
+using namespace std; 
+
+struct Registro{
+	string NOMBRE;
+	string DNI;
+	string CODIGO;
+	string EDAD;
+	string EMAIL;
+	string SEXO;
+	string TELEFONO;
+};
+
+struct Prestamo {
+    string nombre_libro;
+    string fecha_entrega;
+    string fecha_devolucion;
+};
+
+struct Book {
+    long long id; 
+    string nombre;
+    string autor;
+    int anio;
+    int paginas;
+};
 
 
 #endif
