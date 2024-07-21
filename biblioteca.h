f0a9d2903b563e9b15c9e9443fc3337512e1d347
Guts <aamamanica@unjbg.edu.pe>
2024-07-21 18:02:20 -0500

    Se relleno la funcion que habia sido colocada

--- biblioteca.h
+++ biblioteca.h
@@ -5,5 +5,6 @@ void Menu_Principal();
 void Menu_Usuario();
 void Logo();
 void Registrar(Registro REG[], int &cant_usu); 
+void prestamoLibro(Book libros[], int cantidad, long long buscarID, Prestamo prestamos[], int& indicePrestamos); 
  
 #endif
