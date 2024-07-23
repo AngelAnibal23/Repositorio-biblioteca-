a29f25095dd8557fa7fc370f591a75c7146e0c71
ronald <rchoquehuancar@unjbg.edu.pe>
2024-07-23 10:29:48 -0500

    Modificacion

--- biblioteca.h
+++ biblioteca.h
@@ -4,10 +4,8 @@
 void Menu_Principal();
 void Menu_Usuario();
 void Logo();
-<<<<<<< HEAD
-//void Registrar(Registro REG[], int &cant_usu); 
-=======
 
->>>>>>> fcbb96c2c3c8ea0abf5d65f0c173e408b3a3e141
+//void Registrar(Registro REG[], int &cant_usu); 
+
  
 #endif
