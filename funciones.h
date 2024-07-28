39f4eec5ba85e64f4de9a4a426b57c4038ce8933
Guts <aamamanica@unjbg.edu.pe>
2024-07-28 12:29:08 -0500

    Declarando la funcion en el archivo.h

--- funciones.h
+++ funciones.h
@@ -28,6 +28,7 @@ void mostrar(int numcateg); //FUNCION QUE MUESTRA LIBROS DE UN CATEGORIA EN ESPE
 void Menu_Administrador();
 void Mostrar_usuarios_administradores(Registro_Administrador RE[], Registro_usuario REU[]);
 void Registrar_admin(Registro_Administrador REG[],int &cant_admin);
+void devolverLibro(int& cantidad, int buscarID); 
 void valida(int &entrada);
 void QuitarL();
 void quitarlibro();
